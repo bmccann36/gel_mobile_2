59a0c06847a92e193be6ff050d61870b16c27e6b
Brian McCann <bmccann36@gmail.com>
2018-01-05 14:19:52 -0500

    basic conversion of methods for image analysis into one obj

--- CanvasView.js
+++ CanvasView.js
@@ -3,6 +3,7 @@ import { Image, StyleSheet, View, TouchableOpacity, Text, ScrollView } from 'rea
 import { FileSystem, FaceDetector } from 'expo';
 import Canvas, { Image as CanvasImage, Path2D } from 'react-native-canvas';
 import { StackNavigator } from 'react-navigation'
+import imageInfo from './imageInfo'
 
 
 const CANVAS_H = 200
@@ -11,34 +12,35 @@ const CANVAS_W = 200
 export default class CanvasView extends React.Component {
 
   async componentDidMount() {
-    let cal
-    let canvas = this.canvasRef
     const filesList = await Expo.FileSystem.readDirectoryAsync(`${FileSystem.documentDirectory}photos`)
-    console.log(filesList)
-    const src1 = filesList[filesList.length - 2]
+
+    //CANVAS 1 LOGIC
+    let canvas = this.canvasRef
+    const src1 = filesList[filesList.length - 2] // grab 2nd to last
     let srcImg = `${FileSystem.documentDirectory}photos/${src1}`
     const calData = await drawCanvasAsync(srcImg, canvas)
-    calResult = groupByShade(calData)
-
-    const calRed = calResult.midtone.blueVred
-    const calGreen = calResult.midtone.blueVgreen
-    console.log('bVr', Math.round(calRed))
-    // console.log('bVg', Math.round(calGreen))
+    const calibrate = new imageInfo(calData)
+    const calResult = (calibrate.getTintsByShade('midtones'))
 
     // CANVAS 2 LOGIC
-    let mod;
-    const src2 = filesList[filesList.length - 1]
     let canvas2 = this.canvasRef2
+    const src2 = filesList[filesList.length - 1] // grab last
     let srcImg2 = `${FileSystem.documentDirectory}photos/${src2}`
     const compData = await drawCanvasAsync(srcImg2, canvas2)
-    compResult = groupByShade(compData)
-    const compRed = compResult.midtone.blueVred
-    const compGreen = compResult.midtone.blueVgreen
+    const compare = new imageInfo(compData)
+    const compResult = (compare.getTintsByShade('midtones'))
 
-    console.log('rC',  Math.round(compRed - calRed))
-    // console.log('gC', Math.round(compGreen - calGreen))
+    const result = {
+      blueChange: Math.round(compResult.blue - calResult.blue),
+      greenChange: Math.round(compResult.green - calResult.green),
+      redChange: Math.round(compResult.red - calResult.red)
+    }
+
+    console.log('** CALIBRATION **')
+    console.log(calResult)
+    console.log('** RESULTS **')
+    console.log(result)
   }
-
 
 
   render() {
@@ -53,73 +55,6 @@ export default class CanvasView extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-
-
-function groupByShade(imageData) {
-  const dataLength = Object.keys(imageData).length
-  let shades = {
-    shadows: [],
-    midtone: [],
-    highlights: []
-  }
-  const ignore = []
-  for (let i = 0; i < dataLength; i += 4) {
-    const red = imageData[i]
-    const green = imageData[i + 1]
-    const blue = imageData[i + 2]
-    if (red > 250 || red < 20) {
-      ignore.push(red, green, blue)
-      continue;
-    }
-    else if (red < 100) {
-      shades.shadows.push(red, green, blue)
-    }
-    else if (red < 200) {
-      shades.midtone.push(red, green, blue)
-    }
-    else if (red < 250) {
-      shades.highlights.push(red, green, blue)
-    }
-  }
-  // console.log('highlights length', shades.highlights.length)
-  // console.log('midtone length', shades.midtone.length)
-  // console.log('shadows length', shades.shadows.length)
-  for (let props in shades) {
-    shades[props] = blueShift(shades[props])
-  }
-  return shades
-}
-
-// takes in an array of red, green, blue (no alpha) returns the average lack or surplus of blue compared to red and green
-function blueShift(imageData) {
-  // if(!imageData.length) return 'no data'
-  let tints = { blueVred: [], blueVgreen: [] }
-  for (let i = 0; i < imageData.length; i += 3) {
-    const red = imageData[i]
-    const green = imageData[i + 1]
-    const blue = imageData[i + 2]
-    tints.blueVred.push(blue - red)
-    tints.blueVgreen.push(blue - green)
-  }
-  if (tints.blueVred.length) {
-    const sumRed = tints.blueVred.reduce((a, b) => a + b)
-    tints.blueVred = (sumRed / tints.blueVred.length)
-    const sumGreen = tints.blueVgreen.reduce((a, b) => a + b)
-    tints.blueVgreen = (sumGreen / tints.blueVgreen.length)
-  }
-  else {
-    tints.blueVred = 0
-    tints.blueVgreen = 0
-  }
-  return tints
-}
-
 function drawCanvasAsync(srcImg, canvas) {
   return new Promise(resolve => {
     const image = new CanvasImage(canvas);
@@ -129,7 +64,6 @@ function drawCanvasAsync(srcImg, canvas) {
     image.src = srcImg
     image.addEventListener('load', () => {
       context.drawImage(image, 0, 0, 200, 115);
-      // (1, 1, 398, 228)
       context.getImageData(1, 1, 398, 228)
         .then(dataObj => {
           resolve(dataObj.data)
@@ -138,3 +72,8 @@ function drawCanvasAsync(srcImg, canvas) {
   })
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
